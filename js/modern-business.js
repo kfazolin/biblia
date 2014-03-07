e7cda4eb22dd9ddd81eaea103657698766b0f800
kfazolin <kfazolin@yahoo.com.br>
2014-03-06 21:33:51 -0300

    estrutura diversos

--- js/modern-business.js
+++ js/modern-business.js
@@ -7,5 +7,36 @@ $('.carousel').carousel({
 $('.tooltip-social').tooltip({
   selector: "a[data-toggle=tooltip]"
 });
-
+/*
+$('[data-toggle="popover"]').click(function(){
+	$("#"+this.id).popover({'content': this.id});
+});
+*/
 $('[data-toggle="popover"]').popover({'placement': 'top'});
+ 
+$('[data-toggle="popover"]').on('show.bs.popover', function () {
+  
+  var popover = $("#"+this.id).data('bs.popover');
+   
+  jQuery.ajax({
+    url: "bible/"+this.id+".html",
+	dataType: 'html',
+	error: function (data) {
+	alert(data)
+	},
+    success: function(data) {
+	  popover.options.content = data;
+    },
+    async:false
+  });
+
+   
+  // $("#"+this.id).popover({'content': this.id});
+});
+
+// footer
+$( "#footer" ).load( "include/footer.html");
+
+// menu
+$( "#menu" ).load( "include/menu.html");
+
